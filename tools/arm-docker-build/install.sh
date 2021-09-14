7e7d05f1ca4140b6e9d39f118f397f77dc05e9d5
a-sheikh <aamir0sheikh@gmail.com>
2021-09-14 16:40:39 +0200

    trying to go to foxy

--- tools/arm-docker-build/install.sh
+++ tools/arm-docker-build/install.sh
@@ -3,5 +3,5 @@
 sudo apt install qemu-user-static
 
 cp /usr/bin/qemu-arm-static Docker/ 
-sudo docker image build -t ros_arm:1.0 Docker/
-sudo docker run -it --rm  --name ros_arm_inst -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/Docker/workspace:/mnt/workspace:rw ros_arm:1.0 bash /mnt/workspace/workspace.sh
+sudo docker image build -t ros_arm:1.1 Docker/
+sudo docker run -it --rm  --name ros_arm_inst -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/Docker/workspace:/mnt/workspace:rw ros_arm:1.1 bash /mnt/workspace/workspace.sh
